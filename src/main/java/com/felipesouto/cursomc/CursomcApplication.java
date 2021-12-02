b8ba079554b32db43c1b712f973aeeb49bb4da0b
Felipe Souto <felipihsc@gmail.com>
2021-12-02 00:24:22 -0300

    Criando operacao de instaciacao

--- src/main/java/com/felipesouto/cursomc/CursomcApplication.java
+++ src/main/java/com/felipesouto/cursomc/CursomcApplication.java
@@ -1,13 +1,33 @@
 package com.felipesouto.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.felipesouto.cursomc.domain.Categoria;
+import com.felipesouto.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		 Categoria cat1 = new Categoria(null,"Escritorio 1");
+		 Categoria cat2 = new Categoria(null, "Informatica 2");
+		 
+		 categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		 
+		 
+		
 	}
 
 }
