8f3eb22d423ea3eb6b5b0b628a64cf788c1d74cf
Felipe Souto <felipihsc@gmail.com>
2022-01-03 18:17:02 -0300

    inserindo uma nova categoria com POST

--- src/main/java/com/felipesouto/cursomc/services/CategoriaService.java
+++ src/main/java/com/felipesouto/cursomc/services/CategoriaService.java
@@ -23,5 +23,12 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado ! id:"+id+", Tipo:"+ Categoria.class.getName()));
 	}
+	
+	
+	
+	public Categoria insert(Categoria obj){
+		obj.setId(null);
+		return repo.save(obj);
+	}
 
 }
