e9fc15d9d671d39b49bdb8d608e4862c45953e68
Felipe Souto <felipihsc@gmail.com>
2022-01-18 21:26:39 -0300

    Validacao customizada : email não repetido na atualizacao de cliente

--- src/main/java/com/felipesouto/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/felipesouto/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.felipesouto.cursomc.domain.Cliente;
+import com.felipesouto.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 
 	/**
