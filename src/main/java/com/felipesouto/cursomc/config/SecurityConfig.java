d86be1240af8d3c4802a335dd447776d0c74ad96
Felipe Souto <felipihsc@gmail.com>
2022-04-16 10:57:54 -0300

    Implementando autorização

--- src/main/java/com/felipesouto/cursomc/config/SecurityConfig.java
+++ src/main/java/com/felipesouto/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.felipesouto.cursomc.security.JWTAuthenticationFilter;
+import com.felipesouto.cursomc.security.JWTAuthorizationFilter;
 import com.felipesouto.cursomc.security.JWTUtil;
 
 
@@ -56,7 +57,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	    .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 		.antMatchers(PUBLIC_MATCHERS).permitAll()
 		.anyRequest().authenticated();
-	 http.addFilter(new JWTAuthenticationFilter(authenticationManager(),jwtUtil));  
+	  http.addFilter(new JWTAuthenticationFilter(authenticationManager(),jwtUtil));  
+	  http.addFilter(new JWTAuthorizationFilter(authenticationManager(),jwtUtil, userDetailsService));
 	  http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
