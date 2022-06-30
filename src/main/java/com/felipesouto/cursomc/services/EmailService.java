19a90ca3748ff18bd4bded65dc99fb3fc801f753
felipecarvalho-dev <felipihsc@gmail.com>
2022-06-30 00:33:28 -0300

    Esqueci a senha

--- src/main/java/com/felipesouto/cursomc/services/EmailService.java
+++ src/main/java/com/felipesouto/cursomc/services/EmailService.java
@@ -4,6 +4,7 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.felipesouto.cursomc.domain.Cliente;
 import com.felipesouto.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -17,4 +18,6 @@ public interface EmailService {
 	void sendOrderConfirmationHtmlEmail(Pedido obj);
 	
 	void sendHtmlEmail(MimeMessage msg);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
